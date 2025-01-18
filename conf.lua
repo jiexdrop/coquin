707ea0b36080c94b3daef842719b18ccd394ab80
jiexdrop <jorge.noogueira@gmail.com>
2025-01-18 18:49:30 +0000

    radishes disapear

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(c)
   c.title = "Coquin"
   local window = c.screen or c.window -- love 0.9 renamed "screen" to "window"
-  window.width = 1024
-  window.height = 660
+  window.width = 800
+  window.height = 400
 end
--- main.lua
+++ main.lua
@@ -1,47 +1,76 @@
 -- kissy face
 
-local VIRTUAL_WIDTH = 4096
-local VIRTUAL_HEIGHT = 2640
+local VIRTUAL_WIDTH = 3200
+local VIRTUAL_HEIGHT = 1600
 
 local sx = VIRTUAL_WIDTH / love.graphics.getWidth()
 local sy = VIRTUAL_HEIGHT / love.graphics.getHeight()
 
 local entities = {}
 local spriteBatch, protagonist
+local quadRadish
 
 local x, y = 0, 0
 
 
 function love.load()
   love.graphics.setDefaultFilter("nearest", "nearest")
-  love.graphics.setBackgroundColor(0.05, 0.15, 0.05)
+  love.graphics.setBackgroundColor(0.38, 0.60, 1.0)
   image = love.graphics.newImage("graphics/images.png")
   spriteBatch = love.graphics.newSpriteBatch(image)
   
   local quadPlayer = love.graphics.newQuad(0, 0, 20, 20, image:getDimensions())
-  local quadGrass = love.graphics.newQuad(20, 40, 20, 20, image:getDimensions())
+  local quadGround = love.graphics.newQuad(20, 40, 20, 20, image:getDimensions())
   local quadFence = love.graphics.newQuad(0, 40, 20, 20, image:getDimensions())
   local quadTree = love.graphics.newQuad(40, 40, 20, 20, image:getDimensions())
+  local quadGrass = love.graphics.newQuad(20, 0, 20, 20, image:getDimensions())
+  quadRadish = love.graphics.newQuad(20, 60, 20, 20, image:getDimensions())
   
   local windowWidth, windowHeight = love.graphics.getDimensions()
   
-  for baseY = 0, windowHeight, 40 do
-    for baseX = 0, windowWidth, 40 do
-      local quad = (
-        love.math.random() < .10 and quadTree or
-        love.math.random() < .03 and quadFence or
-        quadGrass
-      )
-      table.insert(entities, {
-        quad = quad,
-        x = baseX + math.random(-12, 12),
-        y = baseY + math.random(-8, 8),
-        serial = #entities
-      })
-    end
+
+  for baseX = 0, 180, 20 do
+    table.insert(entities, {
+      quad = quadGround,
+      x = baseX,
+      y = 80,
+      serial = #entities
+    }) -- ground
   end
+
+  for baseX = 20, 160, 20 do
+    table.insert(entities, {
+      quad = quadGrass,
+      x = baseX,
+      y = 60,
+      serial = #entities
+    }) -- grass
+  end
+
+  table.insert(entities, {
+    quad = quadTree,
+    x = 0,
+    y = 60,
+    serial = #entities
+  })
   
-  protagonist = {quad=quadPlayer, x=0, y=0, serial=#entities}
+  table.insert(entities, {
+    quad = quadTree,
+    x = 180,
+    y = 60,
+    serial = #entities
+  })
+
+  for baseX = 20, 160, 20 do
+    table.insert(entities, {
+      quad = quadRadish,
+      x = baseX, --math.random(1, 8) * 20
+      y = 60,
+      serial = #entities
+    })
+  end
+
+  protagonist = {quad=quadPlayer, x=40, y=60, serial=#entities}
   table.insert(entities, protagonist)
 end
 
@@ -60,8 +89,32 @@ function love.keypressed( key, scancode, isrepeat )
 end
 
 function move(dx, dy)
+  if protagonist.x < 20 then
+    dx = 1
+  end
+  if protagonist.x > 160  then
+    dx = -1
+  end
+
   protagonist.x = protagonist.x + dx * 20
-  protagonist.y = protagonist.y + dy * 20
+end
+
+timer = 0
+
+function love.update(dt)
+  timer = timer + dt
+  
+  if timer >= 3 then    
+    -- radish disappear
+    
+    for i, entity in ipairs(entities) do
+      if entity.quad == quadRadish then
+        table.remove(entities, i)
+      end
+    end
+    
+    timer = 0
+  end
 end
 
 function love.draw()
